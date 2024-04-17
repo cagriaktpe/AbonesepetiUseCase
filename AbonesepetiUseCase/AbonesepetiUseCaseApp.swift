e060381ed331dbd92b95ceaaaf7c287466ee5d98
Samet cagri Aktepe <sametcagriaktepe@gmail.com>
2024-04-17 14:43:16 +0300

    Initial Commit

---
+++ AbonesepetiUseCase/AbonesepetiUseCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbonesepetiUseCaseApp.swift
+//  AbonesepetiUseCase
+//
+//  Created by Samet Çağrı Aktepe on 17.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct AbonesepetiUseCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
